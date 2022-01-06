2288c9af0dd46e12275693f671e6c2f9c1ff0c31
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-06 18:06:41 +0530

    ECOMMERCE-DATABASE : create get post and put method for product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { getAllProduct } = require('../controllers/productController')
+const { getAllProduct, createProduct, updateProduct } = require('../controllers/productController')
 
 const router = express.Router()
 
 router.route('/products').get(getAllProduct)
+router.route("/product/new").post(createProduct);
+router.route('/product/:id').put(updateProduct)
 
 
 module.exports = router
