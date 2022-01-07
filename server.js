0a64ba58fb3e4bbbddb6e61ec619abd9a665f539
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-07 16:05:16 +0530

    build Errorhandler(2) For unhandled Promise rejection,Handling uncaught exception and wrong Mongodb id error

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-const app = require('./app')
+const app = require("./app");
 
-const dotenv = require('dotenv')
-const connectDB = require('./config/database')
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+
+// Handling uncaught exception
+process.on('uncaughtException', (err) =>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to uncaught exception`);
+    process.exit(1)
+})
 
 //config
 dotenv.config({ path: "config/config.env" });
 
 // connecting to database
-connectDB()
+connectDB();
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
 
+// unhandled Promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
