622212f11b614fcc6b6431f45596d0641a3ac7af
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-07 15:50:32 +0530

    build errorHandler and catchAsync handler errors

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,40 +1,40 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 //Create product
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
+
 // get all product
-exports.getAllProduct = async (req, res) => {
+exports.getAllProduct = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
 
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 // get product details
-exports.getProductDetails = async (req, res, next) => {
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({
     success: true,
     product,
   });
-};
+});
 
 // update product
-exports.updateProduct = async (req, res) => {
+exports.updateProduct = catchAsyncErrors(async (req, res) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(500).json({
@@ -51,10 +51,10 @@ exports.updateProduct = async (req, res) => {
     success: true,
     product,
   });
-};
+});
 
 // delete product
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(500).json({
@@ -68,4 +68,4 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product deleted successfully",
   });
-};
+});
