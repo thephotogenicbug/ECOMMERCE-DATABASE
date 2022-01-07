a79ac77317ba22448763ecdfc80f84e69d6e4deb
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-07 17:07:51 +0530

    handle backend search, filter, pagination query's

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 //Create product
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,11 +14,17 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // get all product
 exports.getAllProduct = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
-
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments()
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
   res.status(200).json({
     success: true,
     products,
+    productCount,
   });
 });
 
