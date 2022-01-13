b196cbd75ecf8c7016da2bffc05842e08e67ac3a
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-13 14:59:14 +0530

    implement getAll orders , update Order delete Order

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,14 +3,23 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
-router
-  .route("/order/:id")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/user").get(isAuthenticatedUser, myOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
 
 module.exports = router;
