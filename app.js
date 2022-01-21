e92f300b00296c9cfb6eeb8dfd5c5087569fb3f5
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-21 14:32:18 +0530

    finalize

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = express();
 const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 //Route imports
 const product = require("./routes/productRoute");
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
-
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
+const cloudinary = require("cloudinary");
 
 // Handling uncaught exception
-process.on('uncaughtException', (err) =>{
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to uncaught exception`);
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
+});
 
 //config
 dotenv.config({ path: "config/config.env" });
@@ -16,10 +16,15 @@ dotenv.config({ path: "config/config.env" });
 // connecting to database
 connectDB();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
-
 
 // unhandled Promise rejection
 process.on("unhandledRejection", (err) => {
